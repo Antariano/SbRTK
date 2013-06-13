f7364c45632f8c511ec9a016c379f9a43ba62ec9
LimeByte <limebyte@twf.co.nz>
2013-06-13 16:53:38 +1200

    Use proper Java conventions.

--- src/main/java/net/xereo/sbrtk/SbRTK.java
+++ src/main/java/net/xereo/sbrtk/SbRTK.java
@@ -5,23 +5,18 @@ import com.drdanick.McRKit.module.Module;
 import com.drdanick.McRKit.module.ModuleLoader;
 import com.drdanick.McRKit.module.ModuleMetadata;
 
-public class SbRTK extends Module
-{
-    protected SbRTK(ModuleMetadata moduleMetadata, ModuleLoader moduleLoader, ClassLoader classLoader,
-                       ToolkitEvent toolkitEvent, ToolkitEvent toolkitEvent2)
-    {
-        super(moduleMetadata, moduleLoader, classLoader, toolkitEvent, toolkitEvent2);
+public class SbRTK extends Module {
+    protected SbRTK(ModuleMetadata meta, ModuleLoader loader, ClassLoader classLoader, ToolkitEvent event1, ToolkitEvent event2) {
+        super(meta, loader, classLoader, event1, event2);
     }
 
     @Override
-    protected void onEnable()
-    {
+    protected void onEnable() {
         // TODO Everything!
     }
 
     @Override
-    protected void onDisable()
-    {
-        // TODO EVerything!
+    protected void onDisable() {
+        // TODO Everything!
     }
 }
